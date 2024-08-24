c4a066091cd1d923d776e01acb1d9142f3a8a725
Chih Chien Hsieh <twcch1218@gmail.com>
2024-08-24 22:43:40 +0800

    專案初始化

---
+++ src/main/java/tw/cchs/investsmartiqserver/InvestSmartIqServerApplication.java
@@ -0,0 +1,15 @@
+package tw.cchs.investsmartiqserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestSmartIqServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(InvestSmartIqServerApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/tw/cchs/investsmartiqserver/InvestSmartIqServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.cchs.investsmartiqserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestSmartIqServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
